1993d337786411273048373a1b4c568cd52e2e29
Jeff Boek <jeff@jeffboek.com>
2017-09-01 15:01:09 -0700

    Swift 4 upgrade

--- XCUITest/OpenInFocusTest.swift
+++ XCUITest/OpenInFocusTest.swift
@@ -20,7 +20,7 @@ class OpenInFocusTest : BaseTestCase {
         waitforExistence(element: app.textFields["URLBar.urlText"]) // wait for app.label
         let sharedExtName = app.label.contains("Klar") ? "Firefox Klar" : "Firefox Focus" as String
         
-        XCUIDevice.shared().press(.home)
+        XCUIDevice.shared.press(.home)
         let springboard = XCUIApplication(privateWithPath: nil, bundleID: "com.apple.springboard")!
         waitforExistence(element: springboard.scrollViews.otherElements.icons[sharedExtName])
 
